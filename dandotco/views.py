bbc59008663b7a637ea9a7ab7988cfaf64e3666b
Dan Wright <muscle.crab.13@gmail.com>
2017-11-11 22:34:03 -0800

    basic tag filtering of bolgs

--- dandotco/views.py
+++ dandotco/views.py
@@ -4,11 +4,25 @@ import pytumblr, os
 
 from dandotco import app
 
-# app.static_folder = "dandotco/static"
 
 @app.route('/')
-def index():
+def home():
 	entries = os.path.join(app.static_folder, 'entries.json')
 	with open(entries) as bolgs:
 		data = json.load(bolgs)
 	return render_template('index.html', bolgs=data)
+
+@app.route('/tagged/<tag>')
+def tagged(tag=None):
+	entries = os.path.join(app.static_folder, 'entries.json')
+	tag_filtered = []
+	with open(entries) as bolgs:
+		datas = json.load(bolgs)
+		for index, data in enumerate(datas):
+			for bolg_tag in data['tags']:
+				if bolg_tag == tag:
+					tag_filtered.append(data)
+					print(bolg_tag + " == " + tag)
+				
+
+	return render_template('index.html', bolgs=tag_filtered)
