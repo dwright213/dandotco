831a1745e54265c91ccd30cb908982bb87f5c086
Dan Wright <muscle.crab.13@gmail.com>
2017-11-11 21:12:58 -0800

    basic site layout for desktop and mobile too if you like garbage

--- dandotco/views.py
+++ dandotco/views.py
@@ -1,5 +1,14 @@
+# from datetime import datetime
+from flask import Flask, render_template, request, g, abort, jsonify, json
+import pytumblr, os
+
 from dandotco import app
+
+# app.static_folder = "dandotco/static"
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+	entries = os.path.join(app.static_folder, 'entries.json')
+	with open(entries) as bolgs:
+		data = json.load(bolgs)
+	return render_template('index.html', bolgs=data)
