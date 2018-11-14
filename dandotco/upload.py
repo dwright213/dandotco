5f084ee19c588854032a8fdb37f2cd72a9605475
Dan Wright <muscle.crab.13@gmail.com>
2018-11-14 13:08:08 -0800

    image upload seperating processed from original images.

--- dandotco/upload.py
+++ dandotco/upload.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from IPython import embed
 from dandotco import app
+import os
+
+from wand.image import Image
+from wand.display import display
+
 
 app.config['UPLOADED_PHOTOS_DEST'] = 'dandotco/static/img'
 photos = UploadSet('photos', IMAGES)
@@ -13,10 +18,32 @@ def upload(bolg_id=0):
 	if request.method == 'POST' and 'photo' in request.files:
 		bolg_folder = 'original/' + str(bolg_id)
 		filename = photos.save(request.files['photo'], folder=bolg_folder)
-		
+		immmg = edit_image(filename)
 		return render_template('upload.html', img=filename)
 
+def edit_image(image):
+	save_vars = image.split('/')
+	get_path = 'dandotco/static/img/' + image
+	save_path = 'dandotco/static/img/processed/%s/' %(save_vars[1])
+	# print(image)
+	# print(save_vars)
+	# print(get_path)
+	# print(save_path)
+	try:
+		os.mkdir(save_path)
+	except: 
+		print('directory exists, i guess...')
 
+	with Image(filename=(get_path)) as img:
+		print(img.size)
+		for r in 1, 2, 3:
+			with img.clone() as i:
+				i.resize(int(i.width * r * 0.25), int(i.height * r * 0.25))
+				i.rotate(90 * r)
+				i.save(filename=(save_path + save_vars[2]))
+	
+	return 'dandotco/static/img/processed/333/orange.jpg'
+	
 
 
 # image dir established for dev.
