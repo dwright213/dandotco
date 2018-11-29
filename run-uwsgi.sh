83be0f70aaaf5300915b364aaea57cff3cf46699
Dan Wright <muscle.crab.13@gmail.com>
2018-11-28 16:52:25 -0800

    configs

--- run-uwsgi.sh
+++ run-uwsgi.sh
@@ -3,4 +3,6 @@ export ENV=configs/production.py
 export FLASK_APP=dandotco
 export SETTINGS=./settings.cfg
 
-uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app
+# uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app
+
+uwsgi --ini uwsgi.ini
