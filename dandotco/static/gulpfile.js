3cc4b0e1551a76a257320cd3e4c4ce1cccfd5e2e
Dan Wright <muscle.crab.13@gmail.com>
2018-11-29 20:25:54 -0800

    get the thing set up for prod

--- dandotco/static/gulpfile.js
+++ dandotco/static/gulpfile.js
@@ -12,13 +12,41 @@ var
 	// input paths
 	jsInput = './js/main.js',
 	scssInput = './scss/main.scss',
-	svgInput = './img/*.svg',
-
+	svgInput = './img/ui/*',
+	
 	// output paths
-	dist = './dist/';
+	dist = 'dist/',
 
+	// webpack conf
+	webpackConf = {
+		mode: 'production',
+		entry: {
+			home: './js/home.js',
+			bolg: './js/bolg.js',
+			compose: './js/compose.js',
+			edit: './js/edit.js',
+			tagged: './js/tagged.js'
+		},
+		resolve: {},
+		output: {
+			filename: '[name].chunk.js'
+		}
+	},
+
+	vueAlias = {alias: {vue: 'vue/dist/vue.js'}};
+
+
+
+gulp.task('default', function (done) {
+	dist = '/var/media/'
+	webpackConf.mode = 'production'
+	gulp.series('sprites', 'styles', 'scripts')();
+	done();
+})
 
 gulp.task('develop', function() {
+	webpackConf.resolve = vueAlias
+	webpackConf.mode = 'development'
 	gulp.watch(jsGlob, gulp.parallel('scripts'))
 	gulp.watch(scssGlob, gulp.parallel('styles'))
 	gulp.watch(svgGlob, gulp.parallel('sprites'))
@@ -27,34 +55,36 @@ gulp.task('develop', function() {
 
 gulp.task('sprites', function () {
     return gulp.src(svgInput)
-        .pipe(gulp.dest(dist));
+        .pipe(gulp.dest(`${dist}img/ui`));
 });
 
 gulp.task('styles', function () {
   return gulp.src(scssInput)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(dist));
+    .pipe(gulp.dest(`${dist}css/`));
 });
 
 gulp.task('scripts', function() {
 	return gulp.src(jsGlob)
-		.pipe(webpack({
-			mode: 'development',
-			entry: {
-				home: './js/home.js',
-				bolg: './js/bolg.js',
-				compose: './js/compose.js',
-				edit: './js/edit.js',
-				tagged: './js/tagged.js'
-			},
-			output: {
-				filename: '[name].chunk.js'
-			},
-			resolve: {
-				alias: {
-					vue: 'vue/dist/vue.js'
-				}
-			}
-		}))
-		.pipe(gulp.dest(dist));
+		.pipe(webpack(webpackConf
+		// {
+		// 	mode: 'development',
+		// 	entry: {
+		// 		home: './js/home.js',
+		// 		bolg: './js/bolg.js',
+		// 		compose: './js/compose.js',
+		// 		edit: './js/edit.js',
+		// 		tagged: './js/tagged.js'
+		// 	},
+		// 	output: {
+		// 		filename: '[name].chunk.js'
+		// 	},
+		// 	resolve: {
+		// 		alias: {
+		// 			vue: 'vue/dist/vue.js'
+		// 		}
+		// 	}
+		// }
+		))
+		.pipe(gulp.dest(`${dist}js/`));
 });
