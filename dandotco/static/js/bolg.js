929ac51c9ef55996a0f310dc9c6776391877703d
Dan Wright <muscle.crab.13@gmail.com>
2018-12-05 19:29:52 -0800

    wrap those images in figure tags, with javascript

--- dandotco/static/js/bolg.js
+++ dandotco/static/js/bolg.js
@@ -1,27 +1,29 @@
-// element that will be wrapped
 var imgs = Array.from(document.getElementsByTagName('img'));
 
-// let myFirstPromise = new Promise((resolve, reject) => {
-// 	// We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
-// 	// In this example, we use setTimeout(...) to simulate async code. 
-// 	// In reality, you will probably be using something like XHR or an HTML5 API.
-
-// 	document.getElementsByTagName('img')
-
-// 	setTimeout(function() {
-// 		resolve("Success!"); // Yay! Everything went well!
-// 	}, 250);
-
-// });
-
 for (let img of imgs) {
-	let 
-		wrapper = document.createElement('figure'),
-		figCap = document.createElement('figcaption');
+	let wrapper = document.createElement('figure');
+	
+	if (img.classList.length > 0) {
+		let classez = Array.from(img.classList)
+		
+		classez.map(klass => {
+			wrapper.classList.add(klass) 
+			img.classList.remove(klass)
+		})
+
+
+	}
 
 	img.parentNode.insertBefore(wrapper, img)
 	wrapper.appendChild(img)
-	wrapper.appendChild(figCap)
-	figCap.innerHTML = img.title
+
+
+	
+	if (img.title.length > 0) {
+		let figCap = document.createElement('figcaption');
+		
+		figCap.innerHTML = img.title
+		wrapper.appendChild(figCap)
+	}
 
 }
