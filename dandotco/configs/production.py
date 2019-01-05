317fd01c3f51f918413d3088114637ac638d3674
Dan Wright <muscle.crab.13@gmail.com>
2019-01-05 15:44:37 -0800

    fix image thumbnail path

--- dandotco/configs/production.py
+++ dandotco/configs/production.py
@@ -4,5 +4,4 @@ ENV='production'
 UPLOADED_PHOTOS_DEST='/var/media/img/'
 
 # for viewing in front end.
-PROCESSED_PHOTOS_DEST='/var/media/img/processed/'
-
+PROCESSED_PHOTOS_DEST='/static/img/processed/'
